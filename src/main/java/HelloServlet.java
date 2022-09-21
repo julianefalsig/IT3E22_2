00b6f1f8fcd15c8d03a72363eea250e2756bcef3
Christian <chrbudtz@gmail.com>
2022-09-21 10:06:11 +0200

    With web-servlet and index.html

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,21 @@
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/api")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Ahoy there mates - yeeha off we go! - and again and again and again");
+        writer.println("query:" + req.getQueryString());
+        writer.close();
+
+    }
+}
